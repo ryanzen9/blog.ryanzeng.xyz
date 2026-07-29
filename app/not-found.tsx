df1f94a0a579bd1bb837821edabf2b6433501a77
Ryan Zeng <rubyceng0326@gmail.com>
2026-07-29 17:57:05 +0800

    feat: pretty not fount page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,10 +1,25 @@
+"use client";
+import FuzzyText from "@/components/FuzzyText";
+
 export default function NotFound() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        404 - Page Not Found
-      </h1>
-      <p className="mb-4">The page you are looking for does not exist.</p>
+      {/* <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        <FuzzyText baseIntensity={0.2} hoverIntensity={0.5} enableHover>
+          404
+        </FuzzyText>
+      </h1> */}
+
+      <FuzzyText
+        baseIntensity={0.2}
+        hoverIntensity={0.5}
+        enableHover
+        className="mb-8 text-2xl font-semibold tracking-tighter w-fit mx-auto"
+      >
+        404
+      </FuzzyText>
+
+      <p className="mb-4">Page Not Found</p>
     </section>
-  )
+  );
 }
